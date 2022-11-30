396d4d60a9a2ce434e1fdd1d413d5cd1c13c6102
Shira Balali <shiraba28@gmail.com>
2022-11-30 22:36:52 +0200

    changed css

--- src/App.js
+++ src/App.js
@@ -5,34 +5,42 @@ import eggs_img from "./images/eggs.jpg"
 import milk_img from "./images/milk.jpg"
 function App() {
   const expense = [
-    { title: 'banana:', amount: '4$', image: banana_img },
-    { title: 'mango:', amount: '6$', image: mango_img },
-    { title: 'eggs:', amount: '5$', image: eggs_img },
-    { title: 'milk:', amount: '1$', image: milk_img },
+    { title: 'banana', amount: '4$', image: banana_img },
+    { title: 'mango', amount: '6$', image: mango_img },
+    { title: 'eggs', amount: '5$', image: eggs_img },
+    { title: 'milk', amount: '1$', image: milk_img },
   ];
   return (
-    <div>
-      <h2>my shopping list:</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        image={expense[0].image}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        image={expense[1].image}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        image={expense[2].image}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        image={expense[3].image}
-      ></ExpenseItem>
+    <div class="container">
+      <div class="row">
+        <div class="col-2">
+        </div>
+        <div class="col-8">
+          <h2>my shopping list:</h2>
+          <ExpenseItem
+            title={expense[0].title}
+            amount={expense[0].amount}
+            image={expense[0].image}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expense[1].title}
+            amount={expense[1].amount}
+            image={expense[1].image}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expense[2].title}
+            amount={expense[2].amount}
+            image={expense[2].image}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expense[3].title}
+            amount={expense[3].amount}
+            image={expense[3].image}
+          ></ExpenseItem>
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
     </div>
   );
 }
